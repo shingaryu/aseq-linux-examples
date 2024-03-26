5e5778c4a6d37532a9fe14e5934e9a52d5a3c7e7
shingaryu <shingaryu@gmail.com>
2024-03-26 23:27:45 +0900

    Fix build error with MSVC compiler

--- library/headers/stdbool.h
+++ library/headers/stdbool.h
@@ -9,7 +9,7 @@
 #define true    1
 
 #define bool    _Bool
-#if __STDC_VERSION__ < 199901L && __GNUC__ < 3
+#if __STDC_VERSION__ < 199901L && defined(__GNUC__) && __GNUC__ < 3
 typedef int     _Bool;
 #endif
 
